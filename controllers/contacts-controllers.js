080174c5471875b106119d737b34c7e45acba2a5
Marat Baikin <3371335@gmail.com>
2023-04-18 11:24:26 +0300

    added filter for favorite=true

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -5,11 +5,16 @@ const { Contact } = require("../models/contact");
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
   const { page = 1, limit = 10 } = req.query;
+  const { favorite = true } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  }).populate("owner", "name email");
+  const result = await Contact.find(
+    { owner, favorite },
+    "-createdAt -updatedAt",
+    {
+      skip,
+      limit,
+    }
+  ).populate("owner", "name email");
   res.json(result);
 };
 
